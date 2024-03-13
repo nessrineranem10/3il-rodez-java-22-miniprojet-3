e67042e0d3121f5aec1cdfa095444e8660c9e4a6
nessrineranem10 <ranemn@3il.fr>
2024-03-13 11:08:33 +0100

    Ajout de la javadoc pour les tests

--- Test/ModeleTest.java
+++ Test/ModeleTest.java
@@ -1,10 +1,16 @@
 import fr.ecole3il.rodez2023.Modele;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
 
+/**
+ * Cette classe contient des tests pour la classe Modele.
+ */
 public class ModeleTest {
+
+    /**
+     * Test de la méthode getDefinition de la classe Modele.
+     * Cette méthode vérifie si la méthode getDefinition retourne correctement la définition du mot "agenda".
+     */
     @Test
     public void getDefinitionTest() {
         Modele modele = new Modele();
@@ -13,6 +19,10 @@ public class ModeleTest {
         System.out.println(modele.getDefinition(mot));
     }
 
+    /**
+     * Test de la méthode getMot de la classe Modele.
+     * Cette méthode vérifie si la méthode getMot retourne correctement le mot "agenda".
+     */
     @Test
     public void getMotTest() {
         Modele modele = new Modele();
